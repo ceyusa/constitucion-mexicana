dc21fd2532b57a0ce0f0fa41b1863be61684ea34
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2025-01-27 18:30:04 +0100

    scripts/setup-venv.sh: añadir pre-commit

--- scripts/setup-venv.sh
+++ scripts/setup-venv.sh
@@ -4,10 +4,13 @@ if [ -d venv ]
 then
     python3 -m venv --upgrade venv
     . venv/bin/activate
+    pip install --upgrade pip
     pip install --upgrade -r requirements.txt
+    pre-commit autoupdate
 else
     python -m venv venv
     . venv/bin/activate
     pip install --upgrade pip
     pip install -r requirements.txt
+    pre-commit install
 fi
